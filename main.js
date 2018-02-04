f3e12d3162a9448a77ddac3f4ae27fc57d3eca03
Nicholas Williams <obinickenobi@Nicholass-MacBook-Pro.local>
2018-02-04 14:34:02 -0500

    updated main.js

--- main.js
+++ main.js
@@ -9,42 +9,36 @@ prompt.start();
 
 game = {
 	wordsWon : 0,
-	guessesRemaining : 10, 
+	guessesRemaining : 9, 
 	currentWrd : null, 
 	startGame : function (wrd){
-		this.resetGuessesRemaining();
+		this.guessRemaining = 10;
 		this.lettersAlreadyGuessed = "";
 		//get a random word from the array
 		this.currentWrd = new Word.Word(gameFile.wordsForGames.wordBank[Math.floor(Math.random()
 			* gameFile.wordsForGames.wordBank.length)].toUpperCase());
-		
 		this.currentWrd.populateLetterObjectArray();
-
 		this.keepPromptingUser();
-
 	},
-	resetGuessesRemaining : function(){
-		this.guessRemaining = 10;
-	},
+	
 	keepPromptingUser : function(){
 		var self = this;
 
 		prompt.get(['guessLetter'], function(err, result) {
 			clear();
 			var promptInputEntered = result.guessLetter.toUpperCase();
-			if (result.guessLetter.length>1 || self.lettersAlreadyGuessed.includes(promptInputEntered) || /[^A-Z]/.test(promptInputEntered)) {
-				console.log('\nUSER ERROR!!!\n');
-				console.log('Please enter one letter only and only a letter that has not been used already');
-				console.log('The letter or space you guessed is: ' + promptInputEntered);
-				console.log('Letters already guessed are '+self.lettersAlreadyGuessed);
-			    console.log('\n\nGuesses remaining: ', self.guessesRemaining);
-		    	console.log(self.currentWrd.wordRender());
-		    	console.log('here are the letters you guessed already: ');
-		    	console.log(self.lettersAlreadyGuessed);
+			if (result.guessLetter.length>1 || self.lettersAlreadyGuessed.includes(promptInputEntered)
+			 || /[^A-Z]/.test(promptInputEntered)) {
+				console.log('\nERROR!!!, ' +
+				'Please enter only one letter that has not been used already' +
+				'\nThe letter or space you guessed is: ' + promptInputEntered +
+				'\nLetters already guessed are '+self.lettersAlreadyGuessed +
+			    '\n\nGuesses remaining: ', self.guessesRemaining + '\n' + 
+			    self.currentWrd.wordRender(), '\nhere are the letters you guessed already: \n', self.lettersAlreadyGuessed);
 				self.keepPromptingUser();
 			}else{
 
-		    console.log('The letter or space you guessed is: ' + promptInputEntered);
+		    console.log('The letter you guessed: ' + promptInputEntered);
 
 		    self.lettersAlreadyGuessed += promptInputEntered;
 		    //check if guess letter was correct
@@ -52,29 +46,26 @@ game = {
 
 		    //if incorrect then lose guess attempt
 		    if (howManyLettersInWordMatched == 0){
-		    	console.log('You guessed wrong!');
+		    	console.log('Please try again!');
 		    	self.guessesRemaining--;
 		    }else{
-		    	console.log('You guessed right!');
+		    	console.log('Correct!');
 	    		if(self.currentWrd.didWeFindTheWord()){
-	    			console.log(self.currentWrd.wordRender());
-			    	console.log('You Won!!!');
+	    			console.log(self.currentWrd.wordRender() +'\n You did it, congratulations!');
 			    	return; //end game
 			    }
 		    }
 
 		    console.log('Guesses remaining: ', self.guessesRemaining);
-		    console.log(self.currentWrd.wordRender());
-		    console.log('here are the letters you guessed already: ');
-		    console.log(self.lettersAlreadyGuessed);
+		   	console.log(self.currentWrd.wordRender() + '\nCurrent letters you have guessed: \n'
+		    	+ self.lettersAlreadyGuessed);
 
 		    if ((self.guessesRemaining > 0) && (self.currentWrd.found == false)){
 		    	self.keepPromptingUser();
 		    }
 		    else if(self.guessesRemaining == 0){
 		    	clear();
-		    	console.log('Game over bro it was ', self.currentWrd.word);
-		    	console.log('Get with the program man');
+		    	console.log('Game over, you word was ', self.currentWrd.word, ', try again later');
 		    }else{
 		    	console.log(self.currentWrd.wordRender());
 		    }
